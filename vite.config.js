e237a1e8ab214013f11921588c6f1793533dd4ac
Racso <oscarhs0576@gmail.com>
2025-06-11 21:03:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import {defineConfig} from "@netlify/vite-plugin";
+import { defineConfig } from "@netlify/vite-plugin";
+import netlify from "@netlify/vite-plugin";
+
 export default defineConfig({
 	plugins: [
-		 plugins: [netlify()],
+		netlify()
 	]
-})
+});
